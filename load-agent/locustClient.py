3e8eada32b07b727f16c9c50a10a506e4a9c158c
Krishna Waske <krishna.waske@ayanworks.com>
2024-11-19 12:18:35 +0530

    chore:env default values pick
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- load-agent/locustClient.py
+++ load-agent/locustClient.py
@@ -21,21 +21,21 @@ SHUTDOWN_TIMEOUT_SECONDS = 10
 READ_TIMEOUT_SECONDS = 120  # stdout feedback
 ERRORS_BEFORE_RESTART = 10
 # How long to wait for verified = true state
-VERIFIED_TIMEOUT_SECONDS = int(os.getenv("VERIFIED_TIMEOUT_SECONDS", 20))
+VERIFIED_TIMEOUT_SECONDS = int(os.getenv("VERIFIED_TIMEOUT_SECONDS", default=20))
 START_PORT = json.loads(os.getenv("START_PORT"))
 END_PORT = json.loads(os.getenv("END_PORT"))
 # Message to send mediator, defaults to "ping"
-MESSAGE_TO_SEND = os.getenv("MESSAGE_TO_SEND", "ping")
+MESSAGE_TO_SEND = os.getenv("MESSAGE_TO_SEND", default="ping")
 
-ISSUER_TYPE = os.getenv("ISSUER_TYPE", "acapy")
-VERIFIER_TYPE = os.getenv("VERIFIER_TYPE", "acapy")
+ISSUER_TYPE = os.getenv("ISSUER_TYPE", default="acapy")
+VERIFIER_TYPE = os.getenv("VERIFIER_TYPE", default="acapy")
 
 RAW_OOB_BOOL = os.getenv("OOB_INVITE")
 if RAW_OOB_BOOL == "False":
     # Handles case when string False passed in (AKA accidentally evals to True)
     OOB_INVITE = False
 else: 
-    OOB_INVITE = bool(os.getenv("OOB_INVITE", False))
+    OOB_INVITE = bool(os.getenv("OOB_INVITE", default=False))
 
 class PortManager:
     def __init__(self):
